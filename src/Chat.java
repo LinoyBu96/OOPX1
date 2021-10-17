2b26c853aef60d0e875c44e58adc0a8676d60925
LinoyBu96 <lbnoww@gmail.com>
2021-10-17 21:17:32 +0300

    first version

---
+++ src/Chat.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Chat {
+
+    public static void main(String[] args) {
+
+        String[][] repliesToIllegalRequest = {
+                {"what ", "say I should say "},
+                {"whaaat ", "say say "}};
+        ChatterBot[] bots = {
+                new ChatterBot("Lilo", repliesToIllegalRequest[0]),
+                new ChatterBot("Stitch", repliesToIllegalRequest[1])};
+
+        Scanner scanner = new Scanner(System.in);
+        String statement = scanner.nextLine();
+        while (true) {
+            for (ChatterBot bot: bots) {
+                statement = bot.replyTo(statement);
+                System.out.print(statement);
+                scanner.nextLine();
+            }
+        }
+    };
+
+}
